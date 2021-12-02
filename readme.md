81f279dfdc8bd6fc0f352f3b6e55e98376d04c21
Malte <malte.kluft@gmail.com>
2021-12-02 17:26:31 +0100

    🎉 Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+To Start
+
+    yarn install
+    
+or
+
+    npm install
+
+yarn dev
+
+if using npm, change package.json scripts to npm not yarn calls
---
+++ src/x-app.ts
@@ -0,0 +1,13 @@
+import { css, html, LitElement } from "lit";
+import { customElement } from "lit-element";
+
+@customElement("x-app")
+export class XApp extends LitElement {
+  static styles = css``;
+
+  render() {
+    return html`
+    
+    `;
+  }
+}
